1ebf03ffb91a5805f62a1b1f91eecf841140408c
Melih Akkas <melh_akkas@hotmail.com>
2023-02-20 16:24:14 +0300

    Update App.js

--- 3.PROPS/App.js
+++ 3.PROPS/App.js
@@ -11,3 +11,17 @@ function App(){
 
 export default App;
 
+///////////////////////////////////2
+import User from "./Components/User"
+import './App.css';
+
+function App(){
+  return (
+    <>
+    <User name="Mehmet" surname="Seven" isLoggedIn={true} age={32}></User>     
+    </>
+  )
+}
+
+export default App;
+
