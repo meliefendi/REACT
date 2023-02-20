70f2d3fe08ff876d126890e708857a17ca542fc6
Melih Akkas <melh_akkas@hotmail.com>
2023-02-20 16:24:42 +0300

    Update User.js

--- 3.PROPS/User.js
+++ 3.PROPS/User.js
@@ -7,3 +7,23 @@ function User(props){
 export default User;
 
 //burda da  parametreyi props girince altındaki verilere de props ekledik. eğer parametreyi veri şeklinde girersek (name, surname, age) gibi alta sadece veri isimlerini yazarsak aynı işleve sahip olur.
+
+//////////////////////////////////////////////2
+function User({ name, surname, age, isLoggedIn, friends }) {
+    return (
+        <>
+            <h1>{isLoggedIn ? `${name} ${surname} (${age})` : "Giriş yapmadınız."}</h1>
+
+            {
+                friends.map((friend) => (
+                    <div key = {friend.id}>
+                        {friend.id} - {friend.name}
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default User;
+
