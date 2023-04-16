0011bd9c321bb6a5acf2ce61e10f056f47646c7a
Melih Akkas <melh_akkas@hotmail.com>
2023-04-16 18:24:31 +0300

    Add files via upload

---
+++ 6.STİLİZASYON/components/A/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import style from"./style.module.css"
+
+function A() {
+    return (
+        <div className={style.header}> A</div>
+        
+           
+    )
+}
+
+export default A;
