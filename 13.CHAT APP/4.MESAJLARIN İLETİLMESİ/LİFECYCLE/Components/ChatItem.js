e5560002e57c0af36de765479435ae497ca64e8d
Melih Akkas <melh_akkas@hotmail.com>
2023-05-07 19:19:14 +0300

    Update ChatItem.js

--- 13.CHAT APP/4.MESAJLARIN İLETİLMESİ/LİFECYCLE/Components/ChatItem.js
+++ 13.CHAT APP/4.MESAJLARIN İLETİLMESİ/LİFECYCLE/Components/ChatItem.js
@@ -3,6 +3,8 @@ import styles from "./styles.module.css"
 
 function ChatItem({ item }) {
     return (
+        
+        // buraya bu koşulu ekleyerek gönderenin kim olduğu doğrulamasını ve ona göre style uygulanması uyguluyoruz.
         <div className={`${styles.chatItem} ${item.fromMe ? styles.right : ""} `} >
             {
                 item.message
