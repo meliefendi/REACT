c0f8ff2d6c427bd7d00f8dd9e3cc9e64ff5286c6
Melih Akkas <melh_akkas@hotmail.com>
2023-05-12 14:19:58 +0300

    Add files via upload

---
+++ 16.DEPLOYMENT/1.SURGE/src/App.js
@@ -0,0 +1,28 @@
+//SURGE SİTESİ BİZE ÜCRETSİZ YAYIN HAKKI VERİYOR. BU İŞLEMLER YAPMAK İÇİN İLK OLARAK SURGE PAKETİNİ KURDUK. DAHA SORNA YENİ REACT DOSYASI KURDUK
+// VE DAHA SONRA NPM RUN BUİLD DEDİK BUİLD DOSYASI OLUŞTURDU. DAHA SORNA BUİLDE GİDİP SURGE DEDİK VE İÇERİSİNDE KURULUM GERÇEKLEŞTİRDİK.DAHA SONRA YAYINA ALDIK.
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          naber
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
