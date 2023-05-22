dae8a90238b342643e8e2237f3bd1e7e58ae5ba0
Melih Akkas <melh_akkas@hotmail.com>
2023-05-22 15:14:29 +0300

    Add files via upload

---
+++ 17. E-COMMERCE(basic)/8.KAYIT İŞLEMLERİ-2 ( REGİSTER OLAN USERIN VERİLERİNİN DATABASE )/src/Signup/index.js
@@ -0,0 +1,107 @@
+
+import React from "react";
+import { Alert, Box, Button, Flex, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react'
+
+import { useFormik } from "formik";
+
+import validationSchema from "./validations";
+
+import { fetchRegister } from "../../../api";
+
+function Signup() {
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            passwordConfirm: "",
+        },
+        validationSchema,
+        onSubmit: async (values, bag) => {
+            try {
+                const registerResponse = await fetchRegister({
+                    email: values.email,
+                    password: values.password,
+                });
+                console.log(registerResponse)
+            } catch (e) {
+                bag.setErrors({ general: e.response.data.message })
+            }
+        },
+
+    });
+    return (
+        <div>
+            <Flex align="center" width="full" justify="center" >
+                <Box pt="10" >
+                    <Box textAlign="center" >
+                        <Heading>
+                            Sign Up
+                        </Heading>
+                    </Box>
+                    <Box my={5}>
+                        {
+                            formik.errors.general && (
+                                <Alert status="error" > {formik.errors.general} </Alert>
+                            )
+                        }
+                        {
+                            formik.errors.email && formik.touched.email && (
+                                <Alert status="error" > {formik.errors.email} </Alert>
+                            )
+                        }
+                        {
+                            formik.errors.password && formik.touched.password && (
+                                <Alert status="error" > {formik.errors.password} </Alert>
+                            )
+                        }
+                        {
+                            formik.errors.passwordConfirm && formik.touched.passwordConfirm && (
+                                <Alert status="error" > {formik.errors.passwordConfirm} </Alert>
+                            )
+                        }
+                    </Box>
+                    <Box my="5" textAlign="left" >
+                        <form onSubmit={formik.handleSubmit} >
+                            <FormControl >
+                                <FormLabel>E-mail</FormLabel>
+                                <Input
+                                    name="email"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.email}
+                                    isInvalid={formik.touched.email && formik.errors.email}
+                                ></Input>
+
+                            </FormControl>
+                            <FormControl mt="4" >
+                                <FormLabel>Password</FormLabel>
+                                <Input
+                                    name="password" type="password"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                    isInvalid={formik.touched.password && formik.errors.password}
+                                ></Input>
+
+                            </FormControl>
+                            <FormControl mt="4" >
+                                <FormLabel>Password Confirm</FormLabel>
+                                <Input
+                                    name="passwordConfirm" type="password"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.passwordConfirm}
+                                    isInvalid={formik.touched.passwordConfirm && formik.errors.passwordConfirm}
+                                ></Input>
+
+                            </FormControl>
+                            <Button type="submit" width="full" mt="4" colorScheme="teal" >Sign Up</Button>
+                        </form>
+                    </Box>
+                </Box>
+            </Flex>
+        </div>
+    )
+}
+
+export default Signup;
---
+++ 17. E-COMMERCE(basic)/8.KAYIT İŞLEMLERİ-2 ( REGİSTER OLAN USERIN VERİLERİNİN DATABASE )/src/api.jsx
@@ -0,0 +1,24 @@
+
+
+import axios from "axios";
+
+export const fetchProductList = async({ pageParam = 0 }) => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
+    
+
+    return data;
+};
+
+export const fetchProduct = async(product_id) => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
+    
+
+    return data;
+};
+
+export const fetchRegister = async(input) => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`, input);
+
+    return data;
+};
+
