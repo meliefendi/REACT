170b27c2fc6ad60d91b84b82249b10c1365f8823
Melih Akkas <melh_akkas@hotmail.com>
2023-05-23 22:02:22 +0300

    Update ProtectedRoute.js

--- 17. E-COMMERCE(basic)/12.PROTECTED ROUTES ( YETKİSİZ KULLANICININ GİRİŞİNİN ENGELİ )/src/pages/ProtectedRoute.js
+++ 17. E-COMMERCE(basic)/12.PROTECTED ROUTES ( YETKİSİZ KULLANICININ GİRİŞİNİN ENGELİ )/src/pages/ProtectedRoute.js
@@ -1,13 +1,16 @@
 
 
 import { React } from "react";
+//YÖNLENDİRME HOOKLARI
 import { Outlet, Navigate } from "react-router-dom";
 
+//KULLANICI LOGİN BİLGİSİ İÇİN CONTEXT BİLGİSİ
 import { useAuth } from "../context/AuthContext";
+
 
 function ProtectedRoute({ element }) {
     const { loggedIn } = useAuth();
-
+//EĞER KULLANICI BİLGİSİ VARSA OUTLET O DA PROFİLE YOKSA ANASAYFAYA YÖNLENDİR
     return loggedIn ? <Outlet/> : <Navigate to="/" />
        
     
