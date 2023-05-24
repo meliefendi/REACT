7e70b6825f00a7c7cb8e8a8f3e5a8f2ff032ff29
Melih Akkas <melh_akkas@hotmail.com>
2023-05-24 21:44:34 +0300

    Add files via upload

---
+++ 17. E-COMMERCE(basic)/15.SEPETE ATMA İŞLEMLERİ-2 ( SEPETE ATMA ÇIKARMA-LS )/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+import Navbar from "./components/Navbar";
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
+import Products from "./pages/Products"
+import ProductDetail from "./pages/ProductDetail";
+import Profile from "./pages/Profile";
+import Basket from "./pages/Basket";
+import Error404 from "./pages/Error404";
+
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <div id="content">
+          <Routes>
+            <Route path='/' exact element={<Products />} />
+            <Route path='/product/:product_id' element={<ProductDetail />} />
+            <Route path='/Signin' element={<Signin />} />
+            <Route path='/Signup' element={<Signup />} />
+
+            <Route path='/Basket' element={<Basket />} />
+
+            <Route path='*' element={<Error404 />} />
+
+
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
