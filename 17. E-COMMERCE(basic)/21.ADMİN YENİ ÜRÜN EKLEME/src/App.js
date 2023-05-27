5f3fec06e31504d0f7491404ba41df6688da4576
Melih Akkas <melh_akkas@hotmail.com>
2023-05-27 23:16:48 +0300

    Add files via upload

---
+++ 17. E-COMMERCE(basic)/21.ADMİN YENİ ÜRÜN EKLEME/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+import Navbar from "./components/Navbar";
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
+import Products from "./pages/Products"
+import ProductDetail from "./pages/ProductDetail";
+import Profile from "./pages/Profile";
+import Basket from "./pages/Basket";
+import Error404 from "./pages/Error404";
+
+import ProtectedAdmin from "./pages/Admin/ProtectedAdmin";
+import AdminHome from "./pages/Admin/Home";
+import Orders from "./pages/Admin/Orders";
+import AdminProducts from "./pages/Admin/Products";
+import AdminProductsDetail from "./pages/Admin/AdminProductDetail/AdminProductDetail";
+import NewProduct from "./pages/Admin/Products/new";
+
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <div id="content">
+          <Routes>
+            <Route path='/' exact element={<Products />} />
+            <Route path='/product/:product_id' element={<ProductDetail />} />
+            <Route path='/Signin' element={<Signin />} />
+            <Route path='/Signup' element={<Signup />} />
+
+            <Route path='/Basket' element={<Basket />} />
+
+            <Route path='*' element={<Error404 />} />
+
+            {/* Outlet */}
+            <Route element={<ProtectedAdmin />}>
+              <Route index path="/Admin" element={<AdminHome />} />
+              <Route path="/Admin/orders" element={<Orders />} />
+              <Route path="/Admin/AdminProducts" element={<AdminProducts />} />
+              <Route path="/admin/products/:product_id" element={<AdminProductsDetail />} />
+              <Route path="/admin/products/new" element={<NewProduct />} />
+            </Route>
+
+
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ 17. E-COMMERCE(basic)/21.ADMİN YENİ ÜRÜN EKLEME/src/api.jsx
@@ -0,0 +1,101 @@
+
+
+import axios from "axios";
+
+//site içerisinde login olmayı sağlayan bölüm
+axios.interceptors.request.use(function (config) {
+
+    const { origin } = new URL(config.url);
+
+    const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
+    const token = localStorage.getItem("access-token");
+
+    if (allowedOrigins.includes(origin)) {
+        config.headers.Authorization = token
+    }
+    return config;
+},
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+
+export const fetchProductList = async ({ pageParam = 0 }) => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
+
+
+    return data;
+};
+//adminproduct
+export const fetchAllProducts = async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product`);
+    return data;
+};
+//admindelete
+export const deleteProduct = async (product_id) => {
+	const { data } = await axios.delete(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
+	return data;
+};
+
+export const fetchProduct = async (product_id) => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
+
+
+    return data;
+};
+//admin ürün kayıt
+export const postProduct = async (input) => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/product`, input);
+
+
+    return data;
+};
+
+export const fetchRegister = async (input) => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`, input);
+
+    return data;
+};
+
+export const fetchLogin = async (input) => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/login`, input);
+
+    return data;
+};
+
+export const fetchMe = async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`);
+
+    return data;
+};
+
+export const fetchLogout = async () => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`,
+        {
+            refresh_token: localStorage.getItem("refresh-token")
+        }
+    );
+
+    return data;
+};
+//orderdan gönderilen bilgi 
+export const postOrder = async (input) => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/order`, input);
+
+    return data;
+};
+
+//admin orders
+export const fetchOrders = async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/order`)
+
+    return data;
+};
+
+//admin edit page
+export const updateProduct = async (input, product_id) => {
+    const { data } = await axios.put(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`, input)
+
+    return data;
+}
